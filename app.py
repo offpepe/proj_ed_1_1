a1d1caad1b63ccb16b56cf9e066632da1a5ab8c7
Alan A. F. Lopes <alan.lopes@meta.com.br>
2025-06-15 16:47:15 -0300

    feat: embbed functions for kdtree, to face recognizing

--- app.py
+++ app.py
@@ -1,39 +1,127 @@
 # app.py
-from fastapi import FastAPI, Query
+
+import os
+from fastapi import FastAPI, Query, UploadFile, Form, HTTPException
 from kdtree_wrapper import lib, Tarv, TReg
-from ctypes import POINTER,c_char
+from ctypes import POINTER, c_char, c_float
 from pydantic import BaseModel
+from deepface import DeepFace
+from PIL import Image
+from io import BytesIO
+from numpy import array, dtype, zeros, ndarray
+from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures
+
+DATASET_PATH = './dataset'
 
 app = FastAPI()
 
 
 class PontoEntrada(BaseModel):
-    lat: float
-    lon: float
+    file: UploadFile
     nome: str
+    
+class QueryInput(BaseModel):
+    embbed: list[float]
+    
+C_FLOAT_ARRAY_128 = c_float * 128
+
+def list_to_c_float_array(float_list: list[float]) -> C_FLOAT_ARRAY_128: # type: ignore
+    return C_FLOAT_ARRAY_128(*float_list[:128])
+
+def get_face_embbed(file: bytes, name: str = "complete_unknown") -> list[C_FLOAT_ARRAY_128]: # type: ignore
+    image = Image.open(file)
+    faces = DeepFace.extract_faces(array(image))
+    embbeds : list = []
+    for face in faces:
+        FA = face['facial_area']
+        croppedFace = image.crop((FA['x'], 
+                       FA['y'],
+                       FA['x'] + FA['w'], 
+                       FA['y'] + FA['h']))
+        representation = DeepFace.represent(array(croppedFace), 'Facenet')
+        if (not len(representation)):
+            print(f"couldn't represate image of {name} as embbed")
+            continue
+        embbeds.append(C_FLOAT_ARRAY_128(
+            *representation[0]['embedding'][:128]
+            ))
+    return embbeds
+
+
+def process_file(directorie, file):
+    file_path = os.path.join(DATASET_PATH, directorie, file)
+    print(f"[{file_path}] Starting process")
+    try:
+        with open(file_path, 'rb') as fileBytes:
+            print(f"[{file_path}] converting to embbed")
+            embbeds = get_face_embbed(BytesIO(fileBytes.read()))
+            points = []
+            for embbed in embbeds:
+                ponto = TReg(embed=embbed, nome=directorie.encode('utf-8')[:99])
+                points.append(ponto)
+            print(f"[{file_path}] image converted")
+            return points, os.path.join(directorie, file)
+    except:
+        return [], None
+
+def load_dataset():
+    inserted_embbeds = []
+    
+    tasks = []
+    for directorie in os.listdir(DATASET_PATH):
+        files = os.listdir(os.path.join(DATASET_PATH, directorie))
+        for file in files:
+            tasks.append((directorie, file))
+    
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        future_to_task = {executor.submit(process_file, directorie, file): (directorie, file) 
+                         for directorie, file in tasks}
+        
+        for future in concurrent.futures.as_completed(future_to_task):
+            points, file_path = future.result()
+            if file_path:
+                for ponto in points:
+                    lib.inserir_ponto(ponto)
+                inserted_embbeds.append(file_path)
+    
+    return inserted_embbeds
+                                   
 
 @app.post("/construir-arvore")
-def constroi_arvore():
+async def constroi_arvore():
     lib.kdtree_construir()
-    return {"mensagem": "Árvore KD inicializada com sucesso."}
+    inserted_embbeds = load_dataset()
+    return {
+        "mensagem": "Árvore KD inicializada com sucesso.",
+        "total_inseridos": len(inserted_embbeds), 
+        "inseridos": inserted_embbeds}
 
 @app.post("/inserir")
-def inserir(ponto: PontoEntrada):
-    nome_bytes = ponto.nome.encode('utf-8')[:99]  # Trunca se necessário
-    novo_ponto = TReg(lat=ponto.lat, lon=ponto.lon, nome=nome_bytes)
-    lib.inserir_ponto(novo_ponto)
-    return {"mensagem": f"Ponto '{ponto.nome}' inserido com sucesso."}
+async def inserir(nome: str = Form(...), file: UploadFile = UploadFile(...)):
+    if (not file.size):
+        raise HTTPException(status_code=400, detail="É necessário incluir um arquivo")
+    fileBytes = await file.read()
+    print(len(fileBytes))
+    embbeds = []
+    try:
+        embbeds = get_face_embbed(BytesIO(fileBytes), nome)
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Nenhum rosto foi encontrado")
+    nome_bytes = nome.encode('utf-8')[:99]  # Trunca se necessário
+    for embbed in embbeds:
+        novo_ponto = TReg(embed=embbed, nome=nome_bytes)
+        lib.inserir_ponto(novo_ponto)
+    return {"mensagem": f"Ponto '{nome}' inserido com sucesso."}
 
-@app.get("/buscar")
-def buscar(lat: float = Query(...), lon: float = Query(...)):
-    query = TReg(lat=lat, lon=lon)
-
-    arv = lib.get_tree()  # Suponha que esta função retorne ponteiro para árvore já construída
-    resultado = lib.buscar_mais_proximo(arv, query)
-
-    return {
-        "lat": resultado.lat,
-        "lon": resultado.lon,
-        "nome": resultado.nome
-    }
-
+@app.post("/buscar")
+async def buscar(embbed: UploadFile):
+    fileBytes = await embbed.read()
+    embbeds = get_face_embbed(BytesIO(fileBytes))
+    resultados : list[dict] = []
+    for embbed in embbeds:
+        query = TReg(embed=embbed)
+        arv = lib.get_tree()  # Suponha que esta função retorne ponteiro para árvore já construída
+        resultado = lib.buscar_mais_proximo(arv, query)
+        resultados.append({ "nome": resultado.nome, "embbed": [float(resultado.embed[i]) for i in range(128)] })
+    return resultados
